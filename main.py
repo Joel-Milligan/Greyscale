093ea0444046217504887996b87bcaa493e9c589
Joel-Milligan <22701252@student.uwa.edu.au>
2020-06-04 17:01:13 +0800

    Working main

---
+++ main.py
@@ -0,0 +1,16 @@
+from skimage import io, data, color
+import numpy as np
+import os
+
+def main():
+    directory = os.fsencode("./Coloured Images")
+
+    for file in os.listdir(directory):
+        filename = os.fsdecode(file)
+        if filename != ".keep":
+            old_img = io.imread(f"./Coloured Images/{filename}")
+            new_img = color.rgb2gray(old_img)
+            io.imsave(f"./Greyscale Images/{filename.split('.')[0]}_grey.{filename.split('.')[1]}", new_img)
+
+if __name__ == "__main__":
+    main()
